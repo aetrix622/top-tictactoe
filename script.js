f2b16a1b0b15596ad834cde53efa80fc9e86029f
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2024-02-29 14:35:00 -0500

    add GameBoard Function

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 const ToeGame = (function() {
     
+    const GameBoard = (function(cellContent) {
+        // create a 2D array for the gameboard. For each square,
+        // add a cell object. 
+        const rows = 3;
+        const cols = 3;
+        const board = [];
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < cols; j++) {
+                board[i].push(cellContent);
+            }
+        }
+
+        const getBoard = () => board; // returns the entire board
+
+        const updateBoard = (row, col, letter) => {
+            // only update when the square is blank
+            // returns true if successful
+            if (!board[row][col].getValue) {
+                board[row][col].value = letter;
+                return true;
+            } else return false;
+        };
+
+        const resetBoard = () => {
+            for (let i of board) {
+                for (let j of i) {
+                    j.reset();
+                }
+            }
+        }
+
+        return {getBoard, updateBoard, resetBoard};
+    })(Square());
+
 })();
